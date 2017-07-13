1ee49dd07e24207fea9062ab532436e99542e3da
blackburnman04 <brendan.rodgers@gmail.com>
2017-07-13 23:49:11 +0100

    Main File with Menu Containing Options to Scrape Proxies
    This is the main scraper file, when run it will prompt the user with a menu containing options for which site they would like to scrape proxies from. Currently only SSLProxies.org is available, but I will soon add support for other proxy sites in the future.

---
+++ proxyscraper.py
@@ -0,0 +1,47 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+list_of_rows = []
+
+def saveproxy():
+    
+    filename = input("Please input name of file to be saved")        
+    with open (filename + '.csv','w') as file:
+        writer=csv.writer(file)
+        writer.writerow(['IP Address','Port', 'Code', 'Country', 'Anonymity', 'Google', 'HTTPS', 'Last Checked'])
+        for row in list_of_rows:
+            writer.writerow(row)
+    print("File Saved Successfully")
+
+def scrapesslproxiesorg():
+
+    url = 'https://www.sslproxies.org/'
+    response = requests.get(url)
+    html = response.content
+
+    soup = BeautifulSoup(html, "lxml")
+    table = soup.find('table', attrs={'id': 'proxylisttable'})
+
+
+    for row in table.findAll('tr'):
+        list_of_cells = []
+        for cell in row.findAll('td'):
+            text = cell.text.replace('&nbsp;', '')
+            list_of_cells.append(text)
+        list_of_rows.append(list_of_cells)
+
+def menu():
+        strs = ('Enter 1 to Scrape Proxies from http://sslproxies.org\n'
+                'Enter 2 to Exit\n' )
+        choice = input(strs)
+        return int(choice) 
+
+while True:          #use while True
+    choice = menu()
+    if choice == 1:
+        scrapesslproxiesorg()
+        saveproxy()
+    elif choice == 2:
+        break
+    
